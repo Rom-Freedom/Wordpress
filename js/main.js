afd9e6764af62134381083fd56e88eff25fee2cb
Rom-Freedom <romeo.dark@mail.ru>
2023-10-28 02:08:14 +0300

    to comlete the gamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/* SMOOTH SCROLLING OF THE MENU */
 $(document).ready(function() {
-
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu__mobile .menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+    /* CLOSING AND OPENING MOBILE MENU */
+    $('.menu__burger').click(function() {
+        $('.menu__mobile .menu').toggle(500);
+        $(this).toggleClass('close');
+    })
 });
